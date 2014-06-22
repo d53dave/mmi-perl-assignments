c99cd542a32059e94445341662040adb4091401c
d53dave <dave@d53dev.net>
2014-06-23 01:44:30 +0200

    added assignment 6

---
+++ assignment6/regexcheck.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use Tk;
+use Tk::LabFrame;
+
+my $mw = MainWindow->new(-title => 'Perl Mini Regex Tester');
+
+my $txtlabframe = $mw->LabFrame(
+    -label => 'Target Text',
+    -labelside => 'acrosstop',
+	)->pack(-side   => 'bottom',
+            -expand => 1,
+            -fill   => 'both',
+           );
+           
+my $regexlabframe = $mw->LabFrame(
+    -label => 'Regex',
+    -labelside => 'acrosstop',
+	)->pack(-side   => 'top',
+            -fill   => 'x',
+           );
+
+my $regexentry = $regexlabframe ->Text(-height => 1,-background => 'white')->pack(-side =>
+'top', -fill => 'x',);
+           
+
+
+my $txt = $txtlabframe->Scrolled ( 'Text',
+                         -width => 50,
+                         -height => 10,
+                         -relief => 'sunken',
+                         -scrollbars => "osoe",
+                         -background => 'white',
+                         -wrap => 'none' ) -> pack(-side => 'bottom', -fill => 'both', -expand => 1);
+
+
+$txt->tagConfigure('foundtag',-foreground => "white", -background => "blue");
+
+$regexentry->bind('<KeyPress>' => sub { highlightText() });
+$txt->bind('<KeyPress>' => sub { highlightText() });
+MainLoop;
+
+sub highlightText
+{	
+	my $regexinput = $regexentry->get('1.0','end-1c');
+	$txt->tagRemove('foundtag', '1.0', 'end');
+	
+	if($regexinput)
+	{
+		my $result = eval 
+		{
+			my $regex = eval { qr/$regexinput/ };
+			unless ($@){
+				$txt->FindAll(-regexp,-case, $regex);
+				$regexentry->configure(-background => 'white');
+				if ($txt->tagRanges('sel')) 
+				{
+					my %startfinish  = $txt->tagRanges('sel');
+					foreach(sort keys %startfinish) 
+					{
+						$txt->tagAdd("foundtag", $_, $startfinish{$_});
+					}
+				$txt->tagRemove('sel', '1.0', 'end');
+				}
+			} else {
+				$regexentry->configure(-background => 'red'); 
+			}
+		}; 
+	}
+}
